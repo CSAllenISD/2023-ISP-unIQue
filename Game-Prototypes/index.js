69f115405063c39ef596b140de48d5ba98d26f7c
FarazPir <faraz.piracha@student.allenisd.org>
2023-03-09 17:12:41 -0600

    Revert "Deleted files that are no longer needed"
    This reverts commit b435e43f52e0b941473448f363829fdb697fc3d7.

---
+++ Game-Prototypes/index.js
@@ -0,0 +1,12 @@
+//performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+//calculate a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
