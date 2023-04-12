b09f0620779b0399b9f042c1f0449d83d971a6b2
Mehak Jay <mehak.jay@student.allenisd.org>
2023-04-12 08:55:41 -0500

    Recipe Generator using Meal DB API

--- food-court/script.js
+++ food-court/script.js
@@ -1,30 +1,71 @@
-const form = document.querySelector('form');
-const recipeContainer = document.querySelector('#recipe');
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const checkedIngredients = [];
-
-    checkboxes.forEach(function(checkbox) {
-	if (checkbox.checked) {
-	    checkedIngredients.push(checkbox.value);
-	}
-    });
-
-    if (checkedIngredients.length === 0) {
-	recipeContainer.innerHTML = 'Please select at least one ingredient.';
+searchBtn.addEventListener("click", () => {
+    let userInp = document.getElementById("user-inp").value;
+    if (userInp.length == 0) {
+	result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
     } else {
-	const recipe = generateRecipe(checkedIngredients);
-	recipeContainer.innerHTML = recipe;
+	fetch(url + userInp)
+	    .then((response) => response.json())
+	    .then((data) => {
+		let myMeal = data.meals[0];
+		console.log(myMeal);
+		console.log(myMeal.strMealThumb);
+		console.log(myMeal.strMeal);
+		console.log(myMeal.strArea);
+		console.log(myMeal.strInstructions);
+		let count = 1;
+		let ingredients = [];
+		for (let i in myMeal) {
+		    let ingredient = "";
+		    let measure = "";
+		    if (i.startsWith("strIngredient") && myMeal[i]) {
+			ingredient = myMeal[i];
+			measure = myMeal[`strMeasure` + count];
+			count += 1;
+			ingredients.push(`${measure} ${ingredient}`);
+		    }
+		}
+		console.log(ingredients);
+
+		result.innerHTML = `
+    <img src=${myMeal.strMealThumb}>
+    <div class="details">
+        <h2>${myMeal.strMeal}</h2>
+        <h4>${myMeal.strArea}</h4>
+    </div>
+    <div id="ingredient-con"></div>
+    <div id="recipe">
+        <button id="hide-recipe">X</button>
+        <pre id="instructions">${myMeal.strInstructions}</pre>
+    </div>
+    <button id="show-recipe">View Recipe</button>
+    `;
+		let ingredientCon = document.getElementById("ingredient-con");
+		let parent = document.createElement("ul");
+		let recipe = document.getElementById("recipe");
+		let hideRecipe = document.getElementById("hide-recipe");
+		let showRecipe = document.getElementById("show-recipe");
+
+		ingredients.forEach((i) => {
+		    let child = document.createElement("li");
+		    child.innerText = i;
+		    parent.appendChild(child);
+		    ingredientCon.appendChild(parent);
+		});
+
+		hideRecipe.addEventListener("click", () => {
+		    recipe.style.display = "none";
+		});
+		showRecipe.addEventListener("click", () => {
+		    recipe.style.display = "block";
+		});
+	    })
+	    .catch(() => {
+		result.innerHTML = `<h3>Invalid Input</h3>`;
+	    });
     }
 });
-
-function generateRecipe(ingredients) {
-    const proteins = ['chicken', 'beef', 'pork'];
-    const veggies = ['carrots', 'peppers', 'broccoli'];
-    const pastas = ['penne', 'spaghetti', 'blah'];
-
-    const protein = getRandomItemFromArray(proteins.filter(function(p))
-					  }
