44b99641e64ae31a12275258f2b532340f8dd940
FarazPir <faraz.piracha@student.allenisd.org>
2023-05-07 17:11:51 -0500

    revamped matching game

--- Game-Prototypes/matching/index.js
+++ Game-Prototypes/matching/index.js
@@ -1,173 +1,186 @@
-//makes array of card objects
-const cardArray = [
+//store elements in constants
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.getElementById("game-container")
+const result= document.getElementById("result")
+const controls = document.getElementById("controls-container")
+
+
+//array of all cards
+const cardList = [
     {
-	name: 'bomb',
-	img: 'images/bomb.png',
+	name: 'bulb',
+	img: 'images/bulb.jpg',
     },
     {
-	name: 'cherry',
-	img: 'images/cherry.png',
+	name: 'cloud',
+	img: 'images/cloud.jpg',
     },
     {
-	name: 'coin',
-	img: 'images/coin.png',
+	name: 'flower',
+	img: 'images/flower.jpg',
     },
     {
-	name: 'diamond',
-	img: 'images/diamond.png',
+	name: 'heart',
+	img: 'images/heart.jpg',
     },
     {
-	name: 'hourglass',
-	img: 'images/hourglass.png',
+	name: 'ice-cream',
+	img: 'images/ice-cream.jpg',
     },
     {
-	name: 'mushroom',
-	img: 'images/mushroom.png',
-    },
-    {
-	name: 'orange',
-	img: 'images/orange.png',
-    },
-    {
-	name: 'potion',
-	img: 'images/potion.png',
-    },
-    {
-	name: 'sword',
-	img: 'images/sword.png',
-    },
-     {
-	name: 'bomb',
-	img: 'images/bomb.png',
-    },
-    {
-	name: 'cherry',
-	img: 'images/cherry.png',
-    },
-    {
-	name: 'coin',
-	img: 'images/coin.png',
-    },
-    {
-	name: 'diamond',
-	img: 'images/diamond.png',
-    },
-    {
-	name: 'hourglass',
-	img: 'images/hourglass.png',
-    },
-    {
-	name: 'mushroom',
-	img: 'images/mushroom.png',
+	name: 'lightning',
+	img: 'images/lightning.jpg',
     },
     {
 	name: 'orange',
-	img: 'images/orange.png',
+	img: 'images/orange.jpg',
     },
     {
-	name: 'potion',
-	img: 'images/potion.png',
+	name: 'peace',
+	img: 'images/peace.jpg',
     },
-    {
-	name: 'sword',
-	img: 'images/sword.png',
-    }
+   
 ]
 
-//Shuffles cards
-cardArray.sort(() => 0.5 - Math.random())
-
-
-//selects span from HTML
-const grid = document.querySelector('#grid')
-const result = document.querySelector('#result')
-result.textContent = 'Score: 0'
-
-let cardsChosen = []
-let cardsChosenIDs = []
-const cardsWon = []
-
-
-function createBoard() {
-    //for each card the image is set to blue, data-id is asigned, and eventListner for click is added
-    cardArray.forEach((currentCard,index) => {
-	const card = document.createElement('img')
-	card.setAttribute('src', 'images/blue.png')
-	card.setAttribute('class', 'cardBorder')
-	card.setAttribute('data-id', index)
-	//goes through flipCard if card is clicked
-	card.addEventListener('click', flipCard)
-	grid.appendChild(card)
-	
-    });
-}
-
-function flipCard() {
-    //ensures only two cards can be flipped at a time
-    if (cardsChosen.length < 2) {
-	const cardID = this.getAttribute('data-id')
-	cardsChosen.push(cardArray[cardID].name)
-	cardsChosenIDs.push(cardID)
-	this.setAttribute('src', cardArray[cardID].img)
-	//console.log(cardsChosen)
-	if (cardsChosen.length === 2) {
-	    //once two cards have been selected it gives the user time to look at the card and then checks for a match
-	    setTimeout(checkMatch, 500)
-	}
+//initial time
+let seconds = 0,
+    minutes = 0;
+//initial moves and win count
+let movesCount = 0,
+    winCount = 0;
+//for timer
+const timeGenerator = () => {
+    seconds += 1;
+    //minutes logic
+    if (seconds >= 60) {
+	minutes += 1;
+	seconds = 0;
     }
+    //format time before displaying (add 0 to single digit numbers)
+    let secondsValue = seconds < 10 ? `0${seconds}` :
+	seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` :
+	minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+    
+};
+
+// For calculating moves
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 }
 
-function checkMatch() {
-    const cards = document.querySelectorAll('#grid img')
-    const card1ID = cardsChosenIDs[0]
-    const card2ID = cardsChosenIDs[1]
-    
-    console.log('Check for match!')
-    
+//generates cards and board
+const matrixGenerator = (cardList, columns = 4, rows = 4) => {
+    let size = (columns * rows)
+    gameContainer.innerHTML = "";
+    //makes array of cards with two of every card
+    let cardValues = [...cardList, ...cardList];
+    //simple shuffle
+    cardValues.sort(() => Math.random() - 0.5);
+    //creates cards
+    for (let i = 0; i < size; i++) 
+	gameContainer.innerHTML += `
+<div class="card-container" data-card-value="${cardValues[i].name}">
+<div class="card-before">?</div>
+<div class="card-after">
+<img src="${cardValues[i].img}" class="cardImage"/></div>
+</div>
+`;
+    //grid
+    gameContainer.style.gridTemplateColumns = `repeat(${columns}, auto)`;
 
-    //unflips cards if same card is chosen
-    if (card1ID == card2ID) {
-	console.log('same card chosen')
-	cards[card1ID].setAttribute('src', 'images/blue.png')
-	cards[card2ID].setAttribute('src', 'images/blue.png')
-    
+    //cards
+    let cards = document.querySelectorAll(".card-container");
+    let firstCard = false;
+    let secondCard = false;
 
-    //checks if cards match or not
-    } else if (cardsChosen[0] == cardsChosen[1]) {
-	console.log('match found')
-	alert('You found a match!')
-	//gets rid of matching cards
-	cards[card1ID].setAttribute('src', 'images/white.png')
-	//cards[card1ID].setAttribute('class', 'hiddenBorder')
-	cards[card2ID].setAttribute('src', 'images/white.png')
-	//cards[card2ID].setAttribute('class', 'hiddenBorder')
-	//removes eventListeners
-	cards[card1ID].removeEventListener('click', flipCard)
-	cards[card2ID].removeEventListener('click', flipCard)
-	cardsWon.push(cardsChosen)
+    //card matching
+    let firstCardValue;
+    cards.forEach((card) => {
+	card.addEventListener("click", () => {
+	 //if selected card isn't matched yet then only run
+    if(!card.classList.contains("matched")) {
+	//flip the card
+	card.classList.add("flipped");
+	//if its firstcard
+	if(!firstCard) {
+	    //current card becomes first card
+	    firstCard = card;
+	    firstCardValue = card.getAttribute("data-card-value");   
     } else {
-	//unflips not matching cards
-	console.log('not a match!')
-	cards[card1ID].setAttribute('src', 'images/blue.png')
-	cards[card2ID].setAttribute('src', 'images/blue.png')
-    }
+	//increment moves since user selected second card
+	movesCounter();
+	secondCard = card;
+	let secondCardValue = card.getAttribute("data-card-value");
+	if(firstCardValue == secondCardValue) {
+	    //if both match added matched class so they'll be ignored now
+	    firstCard.classList.add("matched");
+	    secondCard.classList.add("matched");
+	    //set firstCard to false since next one will be first now
+	    firstCard = false;
+	    //increase winCount since a match happened
+	    winCount += 1;
+	    //ends game if enough matches are won
+	    if (winCount == Math.floor(cardValues.length / 2)) {
+		result.innerHTML = `<h2> You Won</h2>
+<h4>Moves: ${movesCount}</h4>`;
+		stopGame();
+	    };
+	} else {
+	    //if the cards don't match flip them back
+	    let [tempFirst, tempSecond] = [firstCard, secondCard];
+	    firstCard = false;
+	    secondCard = false;
+	    let delay = setTimeout(() => {
+		tempFirst.classList.remove("flipped");
+		tempSecond.classList.remove("flipped");
+	    } , 900);
+	};
+    };
+    };
+	});
+    });
+};
 
-    //makes score 100 times the amount of cardsWon
-    result.textContent = 'Score: ' + (cardsWon.length * 100)
-    //resets cardsChosen
-    cardsChosen = []
-    cardsChosenIDs = []
 
-    //victory message
-    if (cardsWon.length === (cardArray.length/2)) {
-	result.textContent = 'Congrats you did it!!!'
-    }
-}
+//initializes game
+const initializer = () => {
+    result.innerText = "";
+    winCount = 0;
+    matrixGenerator(cardList);
+};
 
-createBoard()
-
-
-
-
-
+//functionality for start and stop buttons
+const startStopGame = () => {
+    let interval;
+    //game starting
+    startButton.addEventListener("click", () => {
+	movesCount = 0;
+	seconds = 0;
+	minutes = 0;
+    //controls and buttons visibility
+	controls.classList.add("hide");
+	stopButton.classList.remove("hide");
+	startButton.classList.add("hide");
+	//start timer
+	interval = setInterval(timeGenerator, 1000);
+	//initial moves
+	moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+    initializer();
+    });
     
+    //game stopping
+    stopButton.addEventListener("click", (stopGame = () => {
+	controls.classList.remove("hide");
+	stopButton.classList.add("hide");
+	startButton.classList.remove("hide");
+	clearInterval(interval);
+    }));
+    
+};
+startStopGame();
